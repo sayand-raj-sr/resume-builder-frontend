15e0e24043975e006628a8d8b77bfea9611a295a
sayand-raj <sayandsr@gmail.com>
2025-11-04 15:03:33 +0530

    url-change

--- src/services/baseurl.js
+++ src/services/baseurl.js
@@ -1,4 +1,5 @@
 //baseurl is used to connect frontend and json server
-const baseUrl='http://localhost:3000'
+// const baseUrl='http://localhost:3000'
+const baseUrl='https://resume-builder-backend-mqkb.onrender.com'
 
 export default baseUrl
